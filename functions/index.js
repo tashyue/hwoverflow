73fae40258824a762e5529e5ca0c5d54669fad2b
Tyler Ashton Yue <tashyue@gmail.com>
2020-01-09 17:01:00 -0800

    remove test hello world function

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,10 @@ const FBAuth = require('./utils/fbAuth');
 const app = express();
 
 const { signup } = require('./handlers/users');
-const {helloWorld } = require('./handlers/helloWorld')
+
 
 //user routes
 app.post('/signup', signup);
-app.post('/helloWorld', helloWorld);
+
 
 exports.api = functions.https.onRequest(app);
