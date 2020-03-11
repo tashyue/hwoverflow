74bf01d9f921bbd3ca57eeaf979506e6527d3354
Jacob <jacobjennings510@gmail.com>
2020-03-11 15:49:43 -0700

    Updated routing to newew pages

--- src/app.js
+++ src/app.js
@@ -1,28 +1,19 @@
 import React, { Component} from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Home from './components/home';
-import LogIn from './components/logIn';
+import Home from './pages/Home'
+import Login from './pages/Login';
 import SignUp from './components/signUp';
-import './style/navBar.css';
 import VideoThumbnail from './components/videoThumbnail';
 import Dashboard from './components/dashboard';
-import NavBar from './components/NavBar';
 import Upload from './pages/Upload';
 
 class App extends Component {
     render() {
         return (
             <Router>
-                {/* <div className="footer">
-                    <h2>About Us</h2>
-                    <h3>
-                        Quick description of our product for 
-                        people to see on the home page of the website
-                    </h3>
-                </div>  */}
                 <Switch>
                     <Route exact path='/' component={Home} />
-                    <Route path='/LogIn' component={LogIn} />
+                    <Route path='/Login' component={Login} />
                     <Route path='/SignUp' component={SignUp} />
                     <Route path='/Dashboard' component = {Dashboard} />
                     <Route path='/Upload' component = {Upload} />
