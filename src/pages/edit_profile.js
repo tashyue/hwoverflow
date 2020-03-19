8854c3679a395b1d3188699fa396bfdeeca5e972
Bobby Haus <bhaus550@gmail.com>
2020-03-19 15:07:11 -0700

    editing profile styles

--- src/pages/edit_profile.js
+++ src/pages/edit_profile.js
@@ -1,6 +1,22 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
+import { Button, Typography } from '@material-ui/core'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
+import { makeStyles } from '@material-ui/core/styles'
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        
+    },
+    content: {
+        textAlign: 'center',
+        marginTop: theme.spacing(8)
+    },
+    button: {
+        marginTop: theme.spacing(3), 
+    }
+}));
 class Edit_Profile extends Component {
     render(){
         return (
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -27,13 +27,16 @@ const Profile = () => {
             <NavBar />
         <div className ="content">
             <div className="profile-container">
-            <h1>User Name</h1>
+                <div align="center">
+                    <h1>User Name</h1>
+                </div>
             </div>
             <div className="picture-container">
-
+                <img src= "profile_placeholder.png" alt= "Add a Profile Picture" />
             </div>
             <div classname="logout">
                 <Link to="/Login">
+                <div align="center">
                 <Button
                     color='primary'
                     variant='contained'
@@ -41,19 +44,23 @@ const Profile = () => {
                     size="large"
                 >
                     logout
-                </Button>   
+                </Button> 
+                </div>  
                 </Link>
             </div>
             <div classname="edit_profile">
                 <Link to="/Edit_Profile">
-                <Button
-                    color='primary'
-                    variant='contained'
-                    className={classes.button}
-                    size="large"
-                >
-                    Edit Profile
-                </Button>                </Link>
+                <div align="center">  
+                    <Button
+                        color='primary'
+                        variant='contained'
+                        className={classes.button}
+                        size="large"
+                    >
+                        Edit Profile
+                    </Button>
+                </div> 
+                </Link>
             </div>
 
         </div>
@@ -66,8 +73,10 @@ const Profile = () => {
                 <span><VideoThumbnail /></span>
             </div>
         </div>
+        <div align="center">
         <div className="info-container">
             <h1>Personal Info</h1>
+            <div align="left">
             <div className="user-info">
                 <h2> School: </h2>
                 <h2> Courses: </h2>
@@ -75,6 +84,8 @@ const Profile = () => {
                 <h3>- course 2</h3>
                 <h3>- course 3</h3>
             </div>
+            </div>
+        </div>
         </div>
     </div>
     )
