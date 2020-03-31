f9fd999e0b4cd73adef36dd2ab1af3c4b1954847
Bobby Haus <bhaus550@gmail.com>
2020-03-31 13:54:41 -0700

    Conditional Rendered Logout and Edit Profile buttons

--- src/pages/edit_profile.js
+++ src/pages/edit_profile.js
@@ -5,7 +5,7 @@ import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = theme => ({
+const useStyles = makeStyles(theme => ({
     root: {
         
     },
@@ -16,89 +16,87 @@ const useStyles = theme => ({
     button: {
         marginTop: theme.spacing(3), 
     }
-});
-class Edit_Profile extends Component {
-    render() {
-    const { classes } = this.props
+}));
+const EditProfile = () => {
+    const classes = useStyles();
     return (
         <React.Fragment>
             <main>       
-        <div className={classes.root}>
-            <NavBar />
-        <div className ="content">
-            <div className="profile-container">
-                <div align="center">
-                    <h1>User Name</h1>
+                <div className={classes.root}>
+                    <NavBar />
+                <div className ="content">
+                    <div className="profile-container">
+                        <div align="center">
+                            <h1>User Name</h1>
+                        </div>
+                    </div>
+                    <div className="change_pic">
+                    <div align="center">
+                        <Button
+                                color='primary'
+                                variant='contained'
+                                className={classes.button}
+                                size="large"
+                            >
+                                Change Profile Picture
+                        </Button> 
+                        </div>                   
+                        </div>            
+                    <div className="picture-container">
+                        <img src= "profile_placeholder.png" alt= "Add a Profile Picture" />
+                    </div>
+                    <div classname="save_changes">
+                        <div align="center">
+                        <Button
+                            color='primary'
+                            variant='contained'
+                            className={classes.button}
+                            size="large"
+                        >
+                        <a href="/Profile">Save Changes</a>
+                        
+                        </Button> 
+                        </div>  
+                    </div>
+                    <div classname="cancel">
+                        <div align="center">  
+                            <Button
+                                color='primary'
+                                variant='contained'
+                                className={classes.button}
+                                size="large"
+                            >
+                            <a href="/Profile">Cancel</a>
+                            </Button>
+                        </div> 
+                    </div>
+
                 </div>
-            </div>
-            <div className="change_pic">
-            <div align="center">
-                <Button
-                        color='primary'
-                        variant='contained'
-                        className={classes.button}
-                        size="large"
-                    >
-                        Change Profile Picture
-                </Button> 
-                </div>                   
-                </div>            
-            <div className="picture-container">
-                <img src= "profile_placeholder.png" alt= "Add a Profile Picture" />
-            </div>
-            <div classname="save_changes">
                 <div align="center">
-                <Button
-                    color='primary'
-                    variant='contained'
-                    className={classes.button}
-                    size="large"
-                >
-                <a href="/Profile">Save Changes</a>
-                
-                </Button> 
-                </div>  
-            </div>
-            <div classname="cancel">
-                <div align="center">  
+                <div className="info-container">
                     <Button
                         color='primary'
                         variant='contained'
                         className={classes.button}
                         size="large"
                     >
-                    <a href="/Profile">Cancel</a>
+                    Edit Information
                     </Button>
-                </div> 
+                    <h1>Personal Info</h1>
+                    <div align="left">
+                    <div className="user-info">
+                        <h2> School: </h2>
+                        <h2> Courses: </h2>
+                        <h3>- course 1</h3>
+                        <h3>- course 2</h3>
+                        <h3>- course 3</h3>
+                    </div>
+                    </div>
+                </div>
+                </div>
             </div>
-
-        </div>
-        <div align="center">
-        <div className="info-container">
-            <Button
-                color='primary'
-                variant='contained'
-                className={classes.button}
-                size="large"
-            >
-            Edit Information
-            </Button>
-            <h1>Personal Info</h1>
-            <div align="left">
-            <div className="user-info">
-                <h2> School: </h2>
-                <h2> Courses: </h2>
-                <h3>- course 1</h3>
-                <h3>- course 2</h3>
-                <h3>- course 3</h3>
-            </div>
-            </div>
-        </div>
-        </div>
-    </div>
-    </main>
+        </main>
     </React.Fragment>
     );
 }
-}
-export default withStyles(useStyles)(Edit_Profile);
+export default withStyles(useStyles)(EditProfile);
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,94 +2,90 @@ import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import VideoThumbnail from "../components/videoThumbnail";
 import '../style/profile.css';
-import {withStyles, Button, Typography } from '@material-ui/core'
+import { withStyles, Button, Typography } from '@material-ui/core'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = theme => ({
-    root: {
-        
-    },
-    content: {
-        textAlign: 'center',
-        marginTop: theme.spacing(8)
+const useStyles = makeStyles(theme => ({
+    root: {       
     },
     button: {
         marginTop: theme.spacing(3), 
     }
-});
+}));
 
-class Profile extends Component {
-    render() {
-    const { classes } = this.props
+const Profile = () => {
+    const classes = useStyles();
+    const ownProfile = true;
+
     return (
-        <React.Fragment>
-            <main>
         <div className={classes.root}>
             <NavBar />
-        <div className ="content">
-            <div className="profile-container">
+                <div className ="content">
+                    <div className="profile-container">
+                        <div align="center">
+                            <Typography variant="h4">User Name</Typography>
+                        </div>
+                    </div>
+                    <div className="picture-container">
+                        <img src= "profile_placeholder.png" alt= "Add a Profile Picture" />
+                    </div>
+                    <div classname="logout">
+                        <div align="center">
+                        {ownProfile && (
+                            <Button
+                                color='primary'
+                                variant='contained'
+                                className={classes.button}
+                                size="large"
+                            >
+                                <a href="/Login">Logout</a>
+                            </Button> 
+                        )}    
+                        </div>  
+                    </div>
+                    <div classname="edit_profile">
+                        <div align="center">
+                        {ownProfile && (  
+                            <Button
+                                color='primary'
+                                variant='contained'
+                                className={classes.button}
+                                size="large"
+                            >
+                            <a href="/Edit_Profile">Edit Profile</a>
+                            </Button>
+                            )}
+                        </div> 
+                    </div>
+
+                </div>
+                <div className="row-container">
+                    <h1>My Posts</h1>
+                    {/* <hr></hr> */}
+                    <div className="video-container">
+                        <span><VideoThumbnail /></span>
+                        <span><VideoThumbnail /></span>
+                        <span><VideoThumbnail /></span>
+                    </div>
+                </div>
                 <div align="center">
-                    <h1>User Name</h1>
+                <div className="info-container">
+                <Typography variant="h4">Personal Info</Typography>
+                    <div align="left">
+                    <div className="user-info">
+                        <Typography variant="h5">School</Typography>
+                        <Typography variant="h5">Courses:</Typography>
+                        <Typography variant="h7">- course 1</Typography>
+                        <Typography variant="h7">- course 2</Typography>
+                        <Typography variant="h7">- course 3</Typography>
+                    </div>
+                    </div>
+                </div>
                 </div>
             </div>
-            <div className="picture-container">
-                <img src= "profile_placeholder.png" alt= "Add a Profile Picture" />
-            </div>
-            <div classname="logout">
-                <div align="center">
-                <Button
-                    color='primary'
-                    variant='contained'
-                    className={classes.button}
-                    size="large"
-                >
-                    <a href="/Login">Logout</a>
-                </Button> 
-                </div>  
-            </div>
-            <div classname="edit_profile">
-                <div align="center">  
-                    <Button
-                        color='primary'
-                        variant='contained'
-                        className={classes.button}
-                        size="large"
-                    >
-                    <a href="/Edit_Profile">Edit Profile</a>
-                    </Button>
-                </div> 
-            </div>
 
-        </div>
-        <div className="row-container">
-            <h1>My Posts</h1>
-            {/* <hr></hr> */}
-            <div className="video-container">
-                <span><VideoThumbnail /></span>
-                <span><VideoThumbnail /></span>
-                <span><VideoThumbnail /></span>
-            </div>
-        </div>
-        <div align="center">
-        <div className="info-container">
-            <h1>Personal Info</h1>
-            <div align="left">
-            <div className="user-info">
-                <h2> School: </h2>
-                <h2> Courses: </h2>
-                <h3>- course 1</h3>
-                <h3>- course 2</h3>
-                <h3>- course 3</h3>
-            </div>
-            </div>
-        </div>
-        </div>
-    </div>
-    </main>
-    </React.Fragment>
-    );
-}
+    )
 }
 export default withStyles(useStyles)(Profile);
